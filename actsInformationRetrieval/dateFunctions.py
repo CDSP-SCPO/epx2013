34e218238bb13a03c51f381faaf188365c15e454
rom117 <Romain.Lalande@gmail.com>
2013-07-18 19:07:25 +0700

    add dateFunctions file

---
+++ actsInformationRetrieval/dateFunctions.py
@@ -0,0 +1,29 @@
+#manipulates dates
+from datetime import date
+
+def splitFrenchFormatDate(dateString):
+	"""
+	FUNCTION
+	split a date ('DD-MM-YYYY') into year, month and day
+	PARAMETERS
+	dateString: date to split
+	RETURN
+	year, month and day of the date
+	"""
+	day=dateString[:2]
+	month=dateString[3:5]
+	year=dateString[6:]
+
+	return year, month, day
+
+
+def dateToIso(year, month, day):
+	"""
+	FUNCTION
+	transform a date to the iso format ('YYYY-MM-DD')
+	PARAMETERS
+	year, month, day: year, month and day of the date to transform
+	RETURN
+	date in the iso format
+	"""
+	return date(int(year), int(month), int(day)).isoformat()
