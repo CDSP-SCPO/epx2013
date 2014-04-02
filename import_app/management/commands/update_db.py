a9b88496b04a214616289b3387a52b3cd15d0fa6
rom117 <Romain.Lalande@gmail.com>
2014-04-02 23:40:53 +0200

    modif in update_db command

--- import_app/management/commands/update_db.py
+++ import_app/management/commands/update_db.py
@@ -70,61 +70,61 @@ class Command(NoArgsCommand):
 
 
 
-        path=os.path.dirname(__file__)+"/attendance_pb.csv"
-        writer=csv.writer(open(path, 'w'))
-        count=0
-#~ #~
-        min_attends=ImportMinAttend.objects.all()
-        for min_attend in min_attends:
-             #fill verbatim table
-            verbatim, created = Verbatim.objects.get_or_create(verbatim=min_attend.verbatim)
-            #fill status table
-            try:
-                status, created = Status.objects.get_or_create(verbatim=verbatim, country=Country.objects.get(country_code=min_attend.country), status=min_attend.status)
-            except Exception, e:
-                pbs=ImportMinAttend.objects.filter(verbatim=verbatim, country=min_attend.country)
-                header=["releve_annee", "releve_mois", "no_ordre", "no_celex", "verbatim", "country", "status"]
-                writer.writerow(header)
-                new_status="AB"
-                #get the status
-                for pb in pbs:
-                    if pb.status!="AB":
-                        new_status=pb.status
-                        print pb.verbatim
-#~ #~
-                        break
-#~ #~
-                #update db
-                count_local=0
-                for pb in pbs:
-                    row=[pb.releve_annee, pb.releve_mois, pb.no_ordre, pb.no_celex, pb.verbatim.encode("utf-8"), pb.country, pb.status]
-                    writer.writerow(row)
-                    if pb.status=="AB":
-                        print pb.releve_annee, pb.releve_mois, pb.no_ordre
-                        print "verbatim", pb.verbatim
-                        print "country", pb.country
-                        print "old status", pb.status
-                        pb.status=new_status
-                        print "new status", pb.status
-                        print ""
-                        pb.save()
-                        count+=1
-                        count_local+=1
-#~ #~
-#~ #~
-                #try to save the status again and check that there is no more errors
-                if status=="AB":
-                    status, created = Status.objects.get_or_create(verbatim=verbatim, country=Country.objects.get(country_code=min_attend.country), status=min_attend.status)
-#~ #~
-                print count_local, "rows locally updated"
-#~ #~
-                writer.writerow([""])
-#~ #~
-        print count, "rows updated"
-#~ #~
-#~ #~
-        #~ #update min_attend for validated acts
-        #~ act_ids=ActIds.objects.filter(src="index", act__validated=2)
-        #~ for act_id in act_ids:
-            #~ print act_id.act
-            #~ link_act_min_attend(act_id)
+        #~ path=os.path.dirname(__file__)+"/attendance_pb.csv"
+        #~ writer=csv.writer(open(path, 'w'))
+        #~ count=0
+#~
+        #~ min_attends=ImportMinAttend.objects.all()
+        #~ for min_attend in min_attends:
+             #~ #fill verbatim table
+            #~ verbatim, created = Verbatim.objects.get_or_create(verbatim=min_attend.verbatim)
+            #~ #fill status table
+            #~ try:
+                #~ status, created = Status.objects.get_or_create(verbatim=verbatim, country=Country.objects.get(country_code=min_attend.country), status=min_attend.status)
+            #~ except Exception, e:
+                #~ pbs=ImportMinAttend.objects.filter(verbatim=verbatim, country=min_attend.country)
+                #~ header=["releve_annee", "releve_mois", "no_ordre", "no_celex", "verbatim", "country", "status"]
+                #~ writer.writerow(header)
+                #~ new_status="AB"
+                #~ #get the status
+                #~ for pb in pbs:
+                    #~ if pb.status!="AB":
+                        #~ new_status=pb.status
+                        #~ print pb.verbatim
+#~
+                        #~ break
+#~
+                #~ #update db
+                #~ count_local=0
+                #~ for pb in pbs:
+                    #~ row=[pb.releve_annee, pb.releve_mois, pb.no_ordre, pb.no_celex, pb.verbatim.encode("utf-8"), pb.country, pb.status]
+                    #~ writer.writerow(row)
+                    #~ if pb.status=="AB":
+                        #~ print pb.releve_annee, pb.releve_mois, pb.no_ordre
+                        #~ print "verbatim", pb.verbatim
+                        #~ print "country", pb.country
+                        #~ print "old status", pb.status
+                        #~ pb.status=new_status
+                        #~ print "new status", pb.status
+                        #~ print ""
+                        #~ pb.save()
+                        #~ count+=1
+                        #~ count_local+=1
+#~
+#~
+                #~ #try to save the status again and check that there is no more errors
+                #~ if status=="AB":
+                    #~ status, created = Status.objects.get_or_create(verbatim=verbatim, country=Country.objects.get(country_code=min_attend.country), status=min_attend.status)
+#~
+                #~ print count_local, "rows locally updated"
+#~
+                #~ writer.writerow([""])
+#~
+        #~ print count, "rows updated"
+#~
+#~
+        #update min_attend for validated acts
+        act_ids=ActIds.objects.filter(src="index", act__validated=2)
+        for act_id in act_ids:
+            print act_id.act
+            link_act_min_attend(act_id)
