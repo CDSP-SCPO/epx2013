6af9bbd18df52e97ca49cf436f8f5009e8bf120f
rom117 <Romain.Lalande@gmail.com>
2014-04-09 12:15:02 +0200

    modif in ImportMinAttend and Status

--- tests/management/commands/check_attendances.py
+++ tests/management/commands/check_attendances.py
@@ -10,7 +10,11 @@ class Command(NoArgsCommand):
 
         #check status errors
         path=settings.PROJECT_ROOT+"/tests/management/commands/"
+        #TEST
+        #~ path="/var/www/europolix_last_server_version/command/management/commands/"
         headers=["releve_annee", "releve_mois", "no_ordre", "no_celex", "verbatim", "country", "status"]
+        nb_importminattend=0
+        nb_status=0
         with open(path+"status_mistakes.csv", 'r') as csv_file:
             status_errors=[]
             header=csv_file.readline()
@@ -22,29 +26,49 @@ class Command(NoArgsCommand):
                 verbatim=row[0].strip()
                 country=row[1].strip()
                 wrong_status=row[2].strip()
-                right_Status=row[3].strip()
+                right_status=row[3].strip()
 
-                #find corresponding acts
+                #find corresponding acts, save new status in ImportMinAttend
                 acts=ImportMinAttend.objects.filter(verbatim=verbatim, country=country, status=wrong_status)
                 for act in acts:
-                    status_errors.append([act.releve_annee, act.releve_mois, act.no_ordre, act.no_celex, act.verbatim.encode("utf-8"), act.country, act.status])
+                    act.status=right_status
+                    act.save()
+                    #~ status_errors.append([act.releve_annee, act.releve_mois, act.no_ordre, act.no_celex, act.verbatim.encode("utf-8"), act.country, act.status])
+                    nb_importminattend+=1
+
+
+                #modify Status table
+                try:
+                    act=Status.objects.get(verbatim=Verbatim.objects.get(verbatim=verbatim), country=Country.objects.get(country_code=country), status=wrong_status)
+                    act.status=right_status
+                    act.save()
+                    nb_status+=1
+                except:
+                    pass
+
+        print "nb_importminattend", nb_importminattend
+        print "nb_status", nb_status
+
+
+
+        #correct errors
 
         #check verbatim <> AB and status= AB
-        wrong_ab=[]
-        wrong_ab.append(["status=AB and verbatim different from AB"])
-        wrong_ab.append(headers)
-        acts=ImportMinAttend.objects.filter(status="AB").exclude(verbatim="AB")
-        for act in acts:
-            wrong_ab.append([act.releve_annee, act.releve_mois, act.no_ordre, act.no_celex, act.verbatim.encode("utf-8"), act.country, act.status])
-
-
+        #~ wrong_ab=[]
+        #~ wrong_ab.append(["status=AB and verbatim different from AB"])
+        #~ wrong_ab.append(headers)
+        #~ acts=ImportMinAttend.objects.filter(status="AB").exclude(verbatim="AB")
+        #~ for act in acts:
+            #~ wrong_ab.append([act.releve_annee, act.releve_mois, act.no_ordre, act.no_celex, act.verbatim.encode("utf-8"), act.country, act.status])
+#~
+#~
         #write results inside a file
-        writer=csv.writer(open(path+"attendance_problems.csv", 'w'),  delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
-
-        for error in status_errors:
-            writer.writerow(error)
-        writer.writerow([""])
-        for error in wrong_ab:
-            writer.writerow(error)
+        #~ writer=csv.writer(open(path+"fix_attendance_problems.csv", 'w'),  delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
+#~
+        #~ for error in status_errors:
+            #~ writer.writerow(error)
+        #~ writer.writerow([""])
+        #~ for error in wrong_ab:
+            #~ writer.writerow(error)
 
 
