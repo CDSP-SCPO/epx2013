e4bed5f59dfd61572c0b6d745b0ed14859da1391
rom117 <romain.lalande@gmail.com>
2014-12-18 19:38:18 +0100

    fix error homepage: no csrf protection??

--- login/views.py
+++ login/views.py
@@ -5,6 +5,11 @@ from django.template import RequestContext
 #use json for the ajax request
 from django.utils import simplejson
 from django.http import HttpResponse
+#logging
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 
 def login_view(request):
@@ -14,10 +19,13 @@ def login_view(request):
     TEMPLATE
     login/index.html
     """
+    logger.debug('\n')
+    logger.debug('login_view')
     logout(request)
     response={}
 
     if request.POST:
+        logger.debug('login_view post')
         #page posted with ajax
         msg=''
         msg_class="error_msg"
@@ -25,8 +33,12 @@ def login_view(request):
         username=request.POST.get('username')
         password=request.POST.get('password')
 
+        logger.debug("user: "+ username)
+        logger.debug("password: "+ password)
+
         user=authenticate(username=username, password=password)
         if user is not None:
+            logger.debug("user is not none", user.username)
             if user.is_active:
                 login(request, user)
                 msg="You're successfully logged in!"
@@ -36,15 +48,19 @@ def login_view(request):
                 response["user"]["username"]=user.username
             else:
                 msg="Your account is not active, please contact the site admin."
+                logger.debug(msg)
         else:
             msg="Your username and/or password are incorrect."
+            logger.debug(msg)
 
         response["msg"]=msg
         response["msg_class"]=msg_class
 
         #transform the data to json so it can be used in jquery
         if request.is_ajax():
+            logger.debug('\n')
             return HttpResponse(simplejson.dumps(response), mimetype='application/json')
 
     #displays the page (GET) or POST if javascript disabled
+    logger.debug('\n')
     return render_to_response('login/index.html', response, context_instance=RequestContext(request))
